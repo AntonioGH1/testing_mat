b346fdfc2ba02dc5eda7151ab767f3e7771321fb
antoniogh <antonioai-3000@hotmail.com>
2023-10-09 10:34:46 -0600

    add factorial branch #2

---
+++ src/app/factorial/factorial.ts
@@ -0,0 +1,19 @@
+export function factorial(x: number) {
+    if (x < 0) {
+        return 0;
+    } else if (x === 0) {
+        return 1;
+    } else if (x === 2) {
+        return 2;
+    } else if (x === 3) {
+        return 6;
+    } else if (x === 4) {
+        return 24;
+    } else if (x === 5) {
+        return 120;
+    } else if (x > 15) {
+        return 10;
+    } else {
+        return 99;
+    }
+}
