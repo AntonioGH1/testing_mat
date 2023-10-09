6341055a3916f53464d36ad8d8aa333d214a7125
antoniogh <antonioai-3000@hotmail.com>
2023-10-09 10:12:57 -0600

    add compute branch #1.2

--- src/app/compute/compute.ts
+++ src/app/compute/compute.ts
@@ -1,3 +1,13 @@
-export function compute(x : number){
-    return -1;
+export function compute(x: number) {
+    if (x < 0) {
+        return 0; // Devolver 0 si x es negativo
+    } else {
+        return x + 1; // Incrementar x en 1 si es positivo
+    }
 }
+
+
+//Código de error
+/*export function compute(x : number){
+    return -1;
+}*/
